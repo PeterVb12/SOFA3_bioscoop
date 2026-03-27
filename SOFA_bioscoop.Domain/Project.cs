68069b0c45ff1952b13a9025eb0de3505255a154
PeterVb12 <peter-veter@outlook.com>
2026-03-27 16:34:16 +0100

    init commit

---
+++ SOFA_bioscoop.Domain/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SOFA_bioscoop.Domain
+{
+    public class Project
+    {
+        public string name;
+        private List<Sprint> sprints = new List<Sprint>();
+
+        public void AddSprint(Sprint sprint)
+        {
+            if (!sprints.Contains(sprint))
+            {
+                sprints.Add(sprint);
+                sprint.SetProject(this);
+            }
+        }
+    }
+}
+
+
+
---
+++ SOFA_bioscoop.Domain/Sprints/Sprint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOFA_bioscoop.Domain
+{
+    public class Sprint
+    {
+        private string _name;
+        private List<BacklogItem> sprintBacklog = new List<BacklogItem>();
+        private ISprintState 
+
+
+    }
+}
+
+
