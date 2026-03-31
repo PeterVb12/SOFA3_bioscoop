d59af3af4ff501566bf41c1577dc5e10a1468ed6
PeterVb12 <peter-veter@outlook.com>
2026-03-31 03:53:23 +0200

    init develop

--- SOFA_bioscoop.Domain/Sprints/SprintState/CreatedState.cs
+++ SOFA_bioscoop.Domain/Sprints/SprintState/CreatedState.cs
@@ -1,11 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SOFA_bioscoop.Domain
     public class CreatedState : ISprintState
+{
+    public void AddBacklogItem(Sprint sprint, BacklogItem item)
     {
+        throw new NotImplementedException();
     }
+
+    public void CancelRelease(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EditSprint(Sprint sprint, string name, DateTime startDate, DateTime endDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void FinishSprint(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void HandlePostFinish(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPipelineFailure(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPipelineSuccess(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void RetryRelease(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StartPipeline(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void StartSprint(Sprint sprint)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UploadReviewSummary(Sprint sprint, Document summary)
+    {
+        throw new NotImplementedException();
+    }
+}
 }
